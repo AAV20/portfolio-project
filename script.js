5a3097712904b438dc7c0e9b03ce916068ee1814
Artur <arthur.hayrapetyan.90@gmail.com>
2022-10-26 16:04:56 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let btn=document.querySelector('.sw-btn');
+btn.addEventListener('click',()=>{
+  if( document.body.classList.contains('darkMode')){
+    document.body.classList.remove('darkMode');
+    btn.textContent ='dark';
+    btn.style = 'background:white;color:black;'
+  }else{
+      document.body.classList.add('darkMode')
+      btn.textContent ='light'
+      btn.style = 'background:black;color:white;'
+  }
+
+})
+
+// const btn = document.querySelector('.sw-btn')
+// btn.addEventListener('click',()=>{
+//   document.body.classList.toggle('darkMode')
+//  })
